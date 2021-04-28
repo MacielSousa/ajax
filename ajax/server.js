f89110a2fe30db2144a185c7c6477e01b2e85adc
Macielsousa <fmacielsousa@gmail.com>
2021-04-28 16:29:56 -0300

    Listando os Estados

--- ajax/server.js
+++ ajax/server.js
@@ -7,7 +7,4 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-//Testando servidor
-app.get('/teste', (req, res) => res.send(new Date))
-    //Mudando a porta do sevidro
 app.listen(8080, () => console.log('Executando'))
